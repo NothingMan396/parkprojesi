1c1021816a993cbccba60d91d6368742c63b01ad
NothingMan396 <81800847+NothingMan396@users.noreply.github.com>
2021-04-20 14:30:24 +0300

    Update park_sens_r_.ino

--- park_sens_r_.ino
+++ park_sens_r_.ino
@@ -1,67 +1,52 @@
-const int trigger_pin = 12;
-const int echo_pin = 13;
-const int buzzer = 2;
-const int mled = 6;  //pinlerimizi tanımladık
-const int yled = 5;
-const int sled = 4;
-const int kled = 3;
+#include <Servo.h>. 
 
-int sure;
-int mesafe;    //sure ve mesafe adlı değişkenler oluşturuyoruz
+const int trigPin = 10;
+const int echoPin = 11;
+
+long duration;
+int distance;
+
+Servo myServo; 
 
 void setup() {
-  pinMode(kled, OUTPUT);
-  pinMode(sled, OUTPUT);  
-  pinMode(yled, OUTPUT);   //ledlerimizi,buzzerımızı ve mesafe sensörümüzün pinlerinden trigpini çıkış olarak tanımladık
-  pinMode(mled, OUTPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(trigger_pin, OUTPUT);
-  pinMode(echo_pin, INPUT); // mesafe sensörümüzün pinlerinden echopini giriş olarak tanımladık
-  Serial.begin(9600); //serial haberleşme başlattık
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
+  Serial.begin(9600);
+  myServo.attach(12); // Servo motor sinyal pini
+}
+void loop() {
+  // 15 derece ile 165 derece arasında dön
+  for(int i=15;i<=165;i++){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  
+  Serial.print(i); 
+  Serial.print(","); 
+  Serial.print(distance); 
+  Serial.print("."); 
+  }
+  
+  for(int i=165;i>15;i--){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+  }
 }
 
-void loop() {
-  digitalWrite(trigger_pin, HIGH);
-  delayMicroseconds(1000);         //trigpinin ölçüm yapabilmesi için her 1 saniyede bir dalga göndermesini sağlıyoruz             
-  digitalWrite(trigger_pin, LOW);  
-  sure = pulseIn(echo_pin, HIGH);
-  mesafe = (sure / 2) / 28.5;//echopinden alınan dalganın gelme süresini cm'e çeviriyoruz
+int calculateDistance(){ 
+  
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
 
-  if (mesafe <= 10)
-  {
-    digitalWrite(kled, HIGH);
-    digitalWrite(buzzer, HIGH);
-    delay(50);                 //mesafe 10 cm'e eşit veya az ise kırmızı led ve buzzera 50 milisaniye enerji ver ve 250 milisaniye  enerji kes
-    digitalWrite(kled, LOW);
-    digitalWrite(buzzer, LOW);
-    delay(250);
-  }
-
-  else if (mesafe <= 25)
-  {
-    digitalWrite(sled, HIGH);
-    digitalWrite(buzzer, HIGH);
-    delay(50);                   //mesafe 25 cm'e eşit veya az ise sarı led ve buzzera 50 milisaniye enerji ver ve 500 milisaniye enerji kes
-    digitalWrite(sled, LOW);
-    digitalWrite(buzzer, LOW);
-    delay(500);
-  }
-
-  else if (mesafe <= 50)
-  {
-    digitalWrite(yled, HIGH);
-    digitalWrite(buzzer, HIGH);
-    delay(50);               //mesafe 50 cm'e eşit veya az ise yeşil led ve buzzera 50 milisaniye enerji ver ve 75050 milisaniye enerji kes
-    digitalWrite(yled, LOW);
-    digitalWrite(buzzer, LOW);
-    delay(750);
-  }
-
-  else
-  {
-    digitalWrite(mled, HIGH);
-    delay(1000);               //mesafe bu değerlerden birisi değil ise her saniye başı mavi led'e enerji ver ve 1 saniye sonra enerji kes
-    digitalWrite(mled, LOW);
-    delay(1000);
-  }
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
+  distance= duration*0.034/2;
+  return distance;
 }
